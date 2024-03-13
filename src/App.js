359b97f788003eeb5733f660dbf7c7a64eb3e7a8
Hryhoreieva Iryna <grigoreva108@gmail.com>
2024-03-13 19:48:01 +0200

    "

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+const cars=[
 
-function App() {
+];
+
+const App=() =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contaner">
+      <h1>Car</h1>
+      <div
+          {cars.map((item.index)=>{
+              return
+              <React.Fragment key=
+              {index}=>
+          <Table
+              brand={item.brand}
+              model={item.model}
+              />
+      </React.Fragment>
+
+
     </div>
   );
 }
 
 export default App;
+
+}
+ 
